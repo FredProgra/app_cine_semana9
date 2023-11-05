ebae1ee33079c0bc8d498f274dcb4b733963c863
fredoProgra <alfredoqooo@gmail.com>
2023-11-05 15:16:32 -0500

    commit init

--- src/main/java/pe/edu/cibertec/appcinecibertectarde/controller/admi/EstadoController.java
+++ src/main/java/pe/edu/cibertec/appcinecibertectarde/controller/admi/EstadoController.java
@@ -3,10 +3,14 @@ package pe.edu.cibertec.appcinecibertectarde.controller.admi;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.cibertec.appcinecibertectarde.model.bd.Estado;
 import pe.edu.cibertec.appcinecibertectarde.repository.EstadoRepository;
+import pe.edu.cibertec.appcinecibertectarde.response.ResultadoResponse;
 import pe.edu.cibertec.appcinecibertectarde.service.EstadoService;
+
+import java.awt.*;
+import java.util.List;
 
 @AllArgsConstructor
 @Controller
@@ -18,5 +22,20 @@ public class EstadoController {
     public String frmestado(Model  model){
 model.addAttribute("listaestados",estadoService.listarEstado());
         return "admi/frmestado";
+    }
+    @PostMapping("/registrar")
+    @ResponseBody
+    public ResultadoResponse registrarEstado(@RequestBody Estado objestado){
+        return estadoService.registrarEstado(objestado);
+    }
+    @GetMapping("/listar")
+    @ResponseBody
+    public List<Estado> ListarEstado(){
+        return estadoService.listarEstado();
+    }
+    @DeleteMapping("/eliminar")
+    @ResponseBody
+    public ResultadoResponse eliminarEstado(@RequestBody Estado objEstado){
+        return estadoService.eliminarEstado(objEstado.getIdestado());
     }
 }
--- src/main/java/pe/edu/cibertec/appcinecibertectarde/service/EstadoService.java
+++ src/main/java/pe/edu/cibertec/appcinecibertectarde/service/EstadoService.java
@@ -6,6 +6,7 @@ import  java.util.List;
 import org.springframework.stereotype.Service;
 import pe.edu.cibertec.appcinecibertectarde.model.bd.Estado;
 import pe.edu.cibertec.appcinecibertectarde.repository.EstadoRepository;
+import pe.edu.cibertec.appcinecibertectarde.response.ResultadoResponse;
 
 
 @Service
@@ -15,11 +16,30 @@ public class EstadoService {
     public List<Estado> listarEstado(){
         return  estadoRepository.findAll();
     }
-    public  boolean registrarEstado(Estado estado){
-        return estadoRepository.save(estado)!=null;
-    }
-    public  void eliminar(Integer idestado){
-        estadoRepository.deleteById(idestado);
-    }
+    public ResultadoResponse registrarEstado(Estado estado){
+        String mensaje="Estado Registrado correctamente";
+        Boolean respuesta=true;
+        if (estado.getIdestado().equals(0)){estado.setIdestado(null);}
+        try {
+            estadoRepository.save(estado);
+        }catch (Exception e){
+            respuesta=false;
+            mensaje="Estado NO registrado";
+        }
 
+        return ResultadoResponse.builder().respuesta(respuesta).mensaje(mensaje).build();
+    }
+    public ResultadoResponse eliminarEstado(Integer idestado){
+        String mensaje="Estado Eliminado Correctamente";
+        Boolean respuesta=true;
+
+        try {
+            estadoRepository.deleteById(idestado);
+        }catch (Exception e){
+            respuesta=false;
+            mensaje="Estado NO registrado";
+        }
+
+        return ResultadoResponse.builder().respuesta(respuesta).mensaje(mensaje).build();
+    }
 }
